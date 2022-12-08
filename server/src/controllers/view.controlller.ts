45989b410e991e9426f567e0579d4ce44094be59
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-12-08 23:11:37 +0100

    Fix: View controller index date fix

--- server/src/controllers/view.controlller.ts
+++ server/src/controllers/view.controlller.ts
@@ -30,13 +30,11 @@ export class ViewController {
      
         const latestMeasurement = serviceResult.value;
 
-        // FIXME: The data type is still corrupeted
-        // TODO: Displat the full date only if the day is different
-        const formatedTimestamp = this.formatDate(latestMeasurement.timestamp);
-        const formatedTemperatureOne = (latestMeasurement.temperatureSensorOne === undefined) ? "Sensor unavailable" : `${latestMeasurement.temperatureSensorOne}°C`;
-        const formatedTemperatureTwo = (latestMeasurement.temperatureSensorTwo === undefined) ? "Sensor unavailable" : `${latestMeasurement.temperatureSensorTwo}°C`;
-        const formatedTemperatureThree = (latestMeasurement.temperatureSensorThree === undefined) ? "Sensor unavailable" : `${latestMeasurement.temperatureSensorThree}°C`;
-        const formatedAir = (latestMeasurement.airContaminationPercentage === undefined) ? "Sensor unavailable" : `${latestMeasurement.airContaminationPercentage}%`;
+        const formatedTimestamp = this.formatDisplayTimestamp(latestMeasurement.timestamp);
+        const formatedTemperatureOne = (latestMeasurement.temperatureSensorOne == undefined) ? "Sensor unavailable" : `${latestMeasurement.temperatureSensorOne}°C`;
+        const formatedTemperatureTwo = (latestMeasurement.temperatureSensorTwo == undefined) ? "Sensor unavailable" : `${latestMeasurement.temperatureSensorTwo}°C`;
+        const formatedTemperatureThree = (latestMeasurement.temperatureSensorThree == undefined) ? "Sensor unavailable" : `${latestMeasurement.temperatureSensorThree}°C`;
+        const formatedAir = (latestMeasurement.airContaminationPercentage == undefined) ? "Sensor unavailable" : `${latestMeasurement.airContaminationPercentage}%`;
 
         response.render("index", {
             time: formatedTimestamp,
@@ -55,9 +53,18 @@ export class ViewController {
         });
     } 
 
-    private formatDate(date: Date): string {
+    private formatDisplayTimestamp(date: Date): string {
         const dateObj = new Date(date);
-        console.log(dateObj);
+
+        const minutes = (dateObj.getMinutes().toString().length === 1) ?
+            `0${dateObj.getMinutes().toString()}` : dateObj.getMinutes().toString();
+
+        const hours = (dateObj.getHours().toString().length === 1) ?
+            `0${dateObj.getHours().toString().length}` : dateObj.getHours().toString().length;
+
+        if (this.isTimestmpFromToday(dateObj)) {
+            return `${ hours }:${ minutes }`;
+        }
 
         const day = (dateObj.getDate().toString().length === 1) ?
             `0${ dateObj.getDate().toString() }` : dateObj.getDate().toString();
@@ -66,12 +73,15 @@ export class ViewController {
             `0${ dateObj.getMonth().toString() }` : dateObj.getMonth().toString();
         const year = dateObj.getFullYear().toString();
         
-        const minutes = (dateObj.getMinutes().toString().length === 1) ?
-            `0${ dateObj.getMinutes().toString() }` : dateObj.getMinutes().toString();
-        
-        const hours = (dateObj.getHours().toString().length === 1) ?
-            `0${ dateObj.getHours().toString().length }` : dateObj.getHours().toString().length;
-
         return `${ hours }:${ minutes } ${ day }-${ month }-${ year }`;
+    }
+
+    private isTimestmpFromToday(date: Date): boolean {
+        const dateObj = new Date(date);
+        const today = new Date(Date.now());
+
+        return dateObj.getDate() === today.getDate() &&
+            dateObj.getMonth() === today.getMonth() &&
+            dateObj.getFullYear() === today.getFullYear();
     }
 }
