713e0e4668ea96ace1426678c71d7c0a6154474f
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-12-13 16:22:51 +0100

    Enh: Old typescript server codebase removed

--- server/src/utilities/date.utility.ts
+++
@@ -1,8 +0,0 @@
-export function isDateCurrentDay(date: Date): boolean {
-    const dateObj = new Date(date);
-    const today = new Date(Date.now());
-
-    return dateObj.getDate() === today.getDate() &&
-        dateObj.getMonth() === today.getMonth() &&
-        dateObj.getFullYear() === today.getFullYear();
-}
