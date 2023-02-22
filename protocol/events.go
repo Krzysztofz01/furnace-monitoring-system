fb5ae64833c427f10412d67125f7146b24b57092
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2023-02-22 17:07:31 +0100

    Enh: Protocol payload wrapper

--- protocol/events.go
+++ protocol/events.go
@@ -1,17 +1,5 @@
 package protocol
 
-import (
-	"errors"
-	"fmt"
-	"strconv"
-	"strings"
-)
-
-const (
-	PayloadSeparatorSymbolRune   rune   = ';'
-	PayloadSeparatorSymbolString string = string(PayloadSeparatorSymbolRune)
-)
-
 type EventType int
 
 const (
@@ -21,26 +9,3 @@ const (
 	DashboardConnectedEvent
 	DashboardDisconnectedEvent
 )
-
-func GetEventTypeFromEventPayload(ep string) (EventType, error) {
-	epParts := strings.Split(ep, PayloadSeparatorSymbolString)
-	if len(epParts) < 2 {
-		return 0, errors.New("protocol: invalid event payload format")
-	}
-
-	eventTypeValue, err := strconv.Atoi(epParts[0])
-	if err != nil {
-		return 0, errors.New("protocol: failed to parse the event type")
-	}
-
-	switch eventTypeValue {
-	case 1, 2, 3, 4, 5:
-		return EventType(eventTypeValue), nil
-	default:
-		return 0, errors.New("protocol: invalid event type identifier provided")
-	}
-}
-
-func EventTypeToString(et EventType) string {
-	return fmt.Sprintf("%d", et)
-}
