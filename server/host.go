20503fee28600d49070d75cd8f45059d6541f4e3
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2023-02-25 13:19:11 +0100

    Fix: Old host mutex code removed

--- server/host.go
+++ server/host.go
@@ -7,29 +7,25 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// TODO: Implement aditional UUID and socket connection validation
+// TODO: Perform tests to verify if the hostpool mutex control is enough.
 type Host struct {
 	id           uuid.UUID
 	socket       *websocket.Conn
 	errorCount   int
 	lastActivity time.Time
-	// mu           sync.Mutex
 }
 
-// TODO: Socket nil check
 func CreateHost(hostId uuid.UUID, socket *websocket.Conn) *Host {
 	return &Host{
 		id:           hostId,
 		socket:       socket,
 		errorCount:   0,
 		lastActivity: time.Now(),
-		// mu:           sync.Mutex{},
 	}
 }
 
 func (h *Host) Send(buffer []byte) error {
-	// h.mu.Lock()
-	// defer h.mu.Unlock()
-
 	err := h.socket.WriteMessage(1, buffer)
 	if err != nil {
 		h.errorCount += 1
@@ -41,9 +37,6 @@ func (h *Host) Send(buffer []byte) error {
 }
 
 func (h *Host) Read() ([]byte, error) {
-	// h.mu.Lock()
-	// defer h.mu.Unlock()
-
 	_, buffer, err := h.socket.ReadMessage()
 	if err != nil {
 		h.errorCount += 1
@@ -55,44 +48,18 @@ func (h *Host) Read() ([]byte, error) {
 }
 
 func (h *Host) BumpErrorCount() bool {
-	// h.mu.Lock()
-	// defer h.mu.Unlock()
-
 	h.errorCount += 1
 	return h.errorCount < maxErrorCount
 }
 
-// func (h *Host) HasInactivityTimeExceeded() bool {
-// 	// h.mu.Lock()
-// 	// defer h.mu.Unlock()
-
-// 	return time.Since(h.lastActivity).Seconds() > float64(maxInactivitySeconds)
-// }
-
 func (h *Host) GetSecondsSinceLastActivity() float64 {
-	// h.mu.Lock()
-	// defer h.mu.Unlock()
-
 	return time.Since(h.lastActivity).Seconds()
 }
 
-func (h *Host) HasErrorCountExceeded() bool {
-	// h.mu.Lock()
-	// defer h.mu.Unlock()
-
-	return h.errorCount > maxErrorCount
-}
-
 func (h *Host) GetErrorCount() int {
-	// h.mu.Lock()
-	// defer h.mu.Unlock()
-
 	return h.errorCount
 }
 
 func (h *Host) Dispose() error {
-	// h.mu.Lock()
-	// defer h.mu.Unlock()
-
 	return h.socket.Close()
 }
