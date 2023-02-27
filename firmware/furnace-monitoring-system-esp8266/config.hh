bae4f0a01e3181368d1d8c41e018ecf89a0987d0
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2023-02-27 22:42:50 +0100

    Fea: Config header file added

---
+++ firmware/furnace-monitoring-system-esp8266/config.hh
@@ -0,0 +1,10 @@
+#ifndef CONFIG_HH
+#define CONFIG_HH
+
+const String FMS_NETWORK_SSID = "";
+const String FMS_NETWORK_PASSWORD = "";
+const String FMS_SERVER_ADDRESS = "";
+const int FMS_SERVER_PORT = 5000;
+const String FMS_HOSTID = "";
+
+#endif // CONFIG_HH
